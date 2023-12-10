a02f32805366c9a528c408bf73a231d864b8df0c
brymmobaggins <ibro2tablet21@yahoo.com>
2023-12-10 21:39:35 +0100

    add API function

--- js/app.js
+++ js/app.js
@@ -1,21 +1,39 @@
-const input = document.getElementById('input-value')
-const searchButton = document.getElementById('search-button')
+// const input = document.getElementById('input-value')
+const button = document.getElementById('search-button')
+
+button.addEventListener("click", searchBooks)
+function searchBooks() {
+    const API_KEY = "AIzaSyAZuWkNYrGWFvy7EF0bble9NXR3bEHXZRk"
+    const searchInput = document.getElementById('search-input').value
 
 
-
-
-
-
-function checkInput() {
-    if (input.value == null) {
-        searchButton.disabled = false
-    } else {
-        searchButton.disabled = true
-    }
+    // make request from Google Book API
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}&key=${API_KEY}`).then(response => response.json())
+        .then(data => displayResult(data))
+        .catch(error => console.error('Error:', error))
 
 }
-searchButton.addEventListener("click", checkInput)
 
-function searchBooks(){
-    const API_KEY = 
+function displayResult(data) {
+    const resultContainer = document.getElementById('result')
+    resultContainer.innerHTML = ""
+
+    if (data.items) {
+        data.items.forEach(book => {
+            const title = book.volumeInfo.title;
+            const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown Authors';
+            const thumbnail = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : '';
+
+            const bookDiv = document.createElement('div');
+            bookDiv.innerHTML = `<div style="float:left; width:33.3%"
+                        <h3>${title}</h3>
+                        <p>Authors: ${authors}</p>
+                        <img src="${thumbnail}" alt="Book Cover">
+                        <hr>
+                    </div>`;
+            resultContainer.appendChild(bookDiv);
+        });
+    } else {
+        resultContainer.innerHTML = '<p>No results found</p>';
+    }
 }
