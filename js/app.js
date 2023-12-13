61951f90dd4f4c91e2572b760d8c71d800ed17a2
brymmobaggins <ibro2tablet21@yahoo.com>
2023-12-13 16:09:18 +0100

    add style and fonts

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
 const button = document.getElementById('search-button')
 
 button.addEventListener("click", searchBooks)
+
 function searchBooks() {
 
     const API_KEY = "AIzaSyAZuWkNYrGWFvy7EF0bble9NXR3bEHXZRk"
     const searchInput = document.getElementById('search-input').value
-
 
     // make request from Google Book API
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}&key=${API_KEY}`).then(response => response.json())
@@ -25,13 +25,15 @@ function displayResult(data) {
             const title = book.volumeInfo.title;
             const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : 'Unknown Authors';
             const thumbnail = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : ' ';
-            const publishedDate = book.volumeInfo.publishedDate ? book.volumeInfo.publishedDate : ' '
+            const publishedDate = book.volumeInfo.publishedDate ? book.volumeInfo.publishedDate : ' ';
+            const previewLink = book.volumeInfo.previewLink || "#"
             const bookDiv = document.createElement('div');
             bookDiv.innerHTML = `
-                        <h3>${title}</h3>
-                        <p>Authors: ${authors}</p>
-                        <p>Date Published: ${publishedDate}</p>
+                        <h3><b>Title:</b> ${title}</h3>
+                        <p><b>Authors:</b> ${authors}</p>
+                        <p><b>Date Published:</b> ${publishedDate}</p>
                         <img src="${thumbnail}" alt="Book Cover">
+                        <p><a href="${previewLink}" target="_blank">More info</p>
                         <hr>
                     `;
             resultContainer.appendChild(bookDiv);
